9c6a68a021852dd1f77f11113ed311131ccd7184
Ayo Ajani <114166504+AyoAjani05@users.noreply.github.com>
2026-02-05 22:06:53 +0100

    feat: define Contact model with state-specific properties

---
+++ dart/lib/models/contact.dart
@@ -0,0 +1,8 @@
+class Contact{
+  final String name;
+  final String phoneNumber;
+  final String category;
+  final String state;
+
+  Contact({required this.name, required this.phoneNumber, required this.category, required this.state});
+}
