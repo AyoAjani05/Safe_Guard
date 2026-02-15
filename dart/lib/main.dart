73ec09db281b2d592ff9a886bd1dfc673f99c087
Ayo Ajani <114166504+AyoAjani05@users.noreply.github.com>
2026-02-15 17:25:53 +0100

    refactor: simplify app setup and switch to system fonts

--- dart/lib/main.dart
+++ dart/lib/main.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'screens/home_page.dart'; // The new Landing Hub with GPS
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(const EmergencyDirectoryApp());
 }
-
 class EmergencyDirectoryApp extends StatelessWidget {
   const EmergencyDirectoryApp({super.key});
 
@@ -18,13 +19,12 @@ class EmergencyDirectoryApp extends StatelessWidget {
       // This sets the "Emergency Red" branding globally as per your project theme
       theme: ThemeData(
         useMaterial3: true,
-        fontFamilyFallback: ['Sans-serif'], // Use system fonts only
+        fontFamily: "Montserrat",
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.red,
           primary: Colors.red,
           secondary: Colors.redAccent,
         ),
-        
         // Customizing the AppBar globally for a consistent look
         appBarTheme: const AppBarTheme(
           centerTitle: true,
@@ -33,7 +33,7 @@ class EmergencyDirectoryApp extends StatelessWidget {
           foregroundColor: Colors.white,
           titleTextStyle: TextStyle(
             fontSize: 20, 
-            fontWeight: FontWeight.bold, 
+            fontWeight: FontWeight.w900, 
             letterSpacing: 1.2
           ),
         ),
